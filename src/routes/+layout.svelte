8bb212c15a6562e6d1481f24e51ffca03c66efc2
emnificent <49172724+emnificent@users.noreply.github.com>
2023-10-24 17:33:34 +0200

    feat: add SEO meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,14 @@
 
   import Header from '../components/Header.svelte';
 	import Footer from '../components/Footer.svelte';
+
+  import logo from '$lib/images/edited_human_rights_logo.png'
 </script>
+
+<svelte:head>
+  <meta name="description" content="The Lenorian calendar is more than just a way to track time — it's a celebration of human rights!" />
+  <meta property="og:image" content="{logo}" />
+</svelte:head>
 
 <Header />
 
